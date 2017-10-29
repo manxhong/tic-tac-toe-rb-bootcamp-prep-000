c432a372e24ec8bbee83c860a759ccf13c1792e4
Man Hong Lee <manhong323@gmail.com>
2017-10-29 04:57:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,6 @@ def turn(board)
   puts "please enter 1-9"
   input = gets.strip
   num = input_to_index(input)
-  player = current_player(board)
   if valid_move?(board, num)
     move(board, num, player)
     display_board(board)
