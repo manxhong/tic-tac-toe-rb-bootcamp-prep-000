2e580b49b01253c2ed274fa8e95ace8bdd2ae361
Man Hong Lee <manhong323@gmail.com>
2017-10-29 04:31:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,5 +15,5 @@ def display_board(board)
   puts rows
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts rows
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
